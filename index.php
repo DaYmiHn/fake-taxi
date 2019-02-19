80ec537f78080b2438e75bacd6fdd15a7cf40b07
Савин Денис <sawi.denis@yandex.ru>
2019-02-19 22:09:06 +0300

    add script

--- index.php
+++ index.php
@@ -1,21 +1,181 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="UTF-8">
-	<title>Такси</title>
-	<link rel="stylesheet" href="style/style.css">
-	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=d40fb052-95ef-400a-b9ad-d8e14749a9be" type="text/javascript"></script>
+    <title>Примеры. Использование маршрутизатора.</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="style/style.css">
+    <script src="https://api-maps.yandex.ru/1.1/index.xml" type="text/javascript"></script>
+    <script type="text/javascript">
+        // Создание обработчика для события window.onLoad
+        YMaps.jQuery(function () {
+            // Создание экземпляра карты и его привязка к созданному контейнеру
+            var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+
+            // Установка для карты ее центра и масштаба
+            map.setCenter(new YMaps.GeoPoint(30.262793,60.019263), 14);
+
+            // Установка элементов управления
+            map.enableScrollZoom();
+
+            // Добавление офиса на карту
+            var myOffice = new YMaps.Placemark(new YMaps.GeoPoint(30.228914,60.023590 ), 14);
+            myOffice.setIconContent("Осейкин");
+            map.addOverlay(myOffice);
+
+            // Менеджер маршрутов
+            var rManager = new RouterManager(map, myOffice.getGeoPoint());
+
+            // При каждом клике на карте пытаемся проложить маршрут
+            YMaps.Events.observe(map, map.Events.Click, function (map, mEvent) {
+                rManager.drawTo(mEvent.getGeoPoint());
+            });
+        });
+
+        // Менеджер маршрутов
+        function RouterManager (map, start) {
+            // Рисование маршрута
+            this.drawTo = function (finish) {
+                // Создание маршрута
+                var router = new YMaps.Router([finish, start]);
+
+                // Установка слушателей для роутера
+                YMaps.Events.observe(router, router.Events.Success, this._success, this);
+                YMaps.Events.observe(router, [router.Events.RouteError, router.Events.Fault], this._fault, this);
+            }
+
+            // Успешное заверешение рисования маршрута
+            this._success = function (router) {
+                // Маршрут
+                var polyline = new YMaps.Polyline(router.getRoute(0).getPoints());
+
+                // Добавляем маршрут на карту
+                map.addOverlay(polyline);
+
+                // Запускаем машинку по карте
+                // После завершения движения удаляем маршрут с карты
+                new Car(router, map).start(function () {
+                    // Удаляем ломанную с карты
+                    map.removeOverlay(polyline);
+                });
+            }
+
+            // Маршрут проложить не удалось
+            this._fault = function () {
+                alert('Не удалось проложить маршрут');
+            }
+        }
+
+        // Машинка
+        function Car (router, map) {
+            var _this = this,
+                points = router.getRoute(0).getPoints(),
+                indexTo = 1,
+                callback;
+
+            // При перемещении карты или смены масштаба поправляем позицию машинки
+            YMaps.Events.observe(map, map.Events.BoundsChange, function () {
+                this.car.stop();
+                this.position();
+            }, this);
+            
+            // Запуск машинки по маршруту
+            this.start = function (func) {
+                this.create(points[0]);
+                this.move();
+                callback = func;
+            }
+
+            // Смена позиции
+            this.position = function () {
+                if (!indexTo) {
+                    indexTo--;
+                }
+
+                // Возвращаем машинку в предыдущую позицию
+                var toPx = map.converter.coordinatesToLocalPixels(points[indexTo]);
+                this.car.css({
+                    left: toPx.x,
+                    top: toPx.y
+                });
+                this.move();
+            }
+
+            // Передвинуть машинку до следующей точки
+            this.move = function () {
+                // Определение точки, в которую необходимо передвинуть машинку
+                var toPx = map.converter.coordinatesToLocalPixels(points[indexTo]);
+
+                // Время за которое нужно проехать
+                var time = points[indexTo - 1].distance(points[indexTo])
+                if (time < 1) {
+                    time = 10;
+                }
+
+                // Перемещение машинки
+                this.car.animate({
+                    left: toPx.x,
+                    top: toPx.y
+                }, {
+                    duration: time, 
+                    complete: function () {
+                        _this.tick();
+                    },
+                    queue: 0
+                });
+            }
+
+            // Действие для машинки
+            this.tick = function () {
+                // Если не доехали до конца маршрута
+                if (indexTo < points.length - 1) {
+                    this.move(); // Едем дальше
+                    indexTo++;
+                } else {
+                    this.remove();
+                }
+            }
+
+            // Создание машинки
+            // startPoint - начало маршрута
+            this.create = function (startPoint) {
+                var fromPx = map.converter.coordinatesToLocalPixels(startPoint);
+
+                // Создание машинки и добавление ее на карту
+                this.car = YMaps.jQuery("<img class=\"car\"/ >")
+                .css({
+                    left: fromPx.x,
+                    top: fromPx.y
+                })
+                .appendTo(map.getContainer());
+            }
+
+            // Удаление машинки
+            this.remove = function () {
+                this.car.remove();
+                
+                // Вызов callback-функции, переданной в метод start()
+                callback.call();
+            }
+        }
+    </script>
+    <style>
+        .car {
+            position: absolute;
+            z-index: 7000;
+            
+            width: 30px;
+            height: 16px;
+            margin-top: -5px;
+            margin-left:-5px;
+            background-image: url(src/123123123.png);
+            background-size: cover;
+        }
+    </style>
 </head>
+
 <body>
-<div id="map"></div>
-<input type="button" id="destroyButton" value="Удалить карту"/>
-
-
-
-
-
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-	<script src="script/script.js"></script>
+    <div id="YMapsID"></div>
 </body>
+
 </html>
--- script/script.js
+++ script/script.js
@@ -1,23 +1,27 @@
-var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
-ymaps.ready(init);
-
-function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [59.941824, 30.315755], // СпБ
-        zoom: 10
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
-
-    document.getElementById('destroyButton').onclick = function () {
-        // Для уничтожения используется метод destroy.
-        myMap.destroy();
-    };
-
-}
+var map;
+		ymaps.ready(function () {
+			map = new ymaps.Map("map-canvas", {
+				center: [56, 37],
+				zoom: 8,
+				type: "yandex#map"
+			});
+			
+			var start_point = [56, 37];
+			var end_point = [57, 39];
+			ymaps.route([start_point, end_point], {
+				mapStateAutoApply: true,
+				avoidTrafficJams: false,
+				multiRoute: false,
+				routingMode: "auto",
+				viaIndexes: []
+			}).then(function (route) {
+				var points = route.getWayPoints();  
+				points.get(0).properties.set('balloonContent', '');
+				points.get(1).properties.set('balloonContent', 'Дистанция: '+route.getHumanLength()+'<br>Продолжительность: '+route.getHumanTime());
+				points.get(0).properties.set('iconContent', 'А');
+				points.get(1).properties.set('iconContent', 'Б');
+				map.geoObjects.add(route);
+			}, function (error) {
+				// Ошибка error.message
+			});
+		});
