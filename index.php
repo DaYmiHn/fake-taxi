a4dc4d8c587b51cc33117534d1d8c8c9bb2bdc0d
Савин Денис <sawi.denis@yandex.ru>
2019-02-19 19:06:46 +0300

    map add

--- index.php
+++ index.php
@@ -3,8 +3,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Такси</title>
+	<link rel="stylesheet" href="style/style.css">
+	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=d40fb052-95ef-400a-b9ad-d8e14749a9be" type="text/javascript"></script>
 </head>
 <body>
-	
+<div id="map"></div>
+<input type="button" id="destroyButton" value="Удалить карту"/>
+
+
+
+
+
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="script/script.js"></script>
 </body>
 </html>
